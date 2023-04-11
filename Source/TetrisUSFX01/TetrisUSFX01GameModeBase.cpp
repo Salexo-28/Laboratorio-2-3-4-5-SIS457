ea671722811f49c7cc50c682bfa05211b158d0f9
Matias Ezequiel Gutierrez Coronado <126731747+Matute04@users.noreply.github.com>
2023-04-11 09:54:49 -0400

    Update TetrisUSFX01GameModeBase.cpp

--- Source/TetrisUSFX01/TetrisUSFX01GameModeBase.cpp
+++ Source/TetrisUSFX01/TetrisUSFX01GameModeBase.cpp
@@ -5,14 +5,12 @@
 //
 void ATetrisUSFX01GameModeBase::BeginPlay()
 {
-    //ciclo que genera bloques
     GetWorldTimerManager().SetTimer(Time_Controller, this, &ATetrisUSFX01GameModeBase::Spawn_Bloque, TIntervalo, true);
 
     Super::BeginPlay();
 
 
 }
-//funcion que genera los bloques
 void ATetrisUSFX01GameModeBase::Spawn_Bloque()
 {
 
@@ -29,47 +27,44 @@ void ATetrisUSFX01GameModeBase::Spawn_Bloque()
         SpawnedPiece->setPositionX(positionX);
         SpawnedPiece->setPositionY(positionY);
         SpawnedPiece->setPositionZ(positionZ);
-            //guarda el nombre y posicion de la pieza
         mpcontenedor.Add(cont_map, SpawnedPiece);
         cont_map++;
     }
     else
     {
-        GetWorldTimerManager().ClearTimer(Time_Controller);//cancela el temporizador
+        GetWorldTimerManager().ClearTimer(Time_Controller);
         GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Orange, TEXT("Tiempo Cumplido"));
-        //muestra el Tmap
-        for (int i = 0; i < cont_map; i++)
+         for (int i = 0; i < cont_map; i++)
         {
             pos_vector = mpcontenedor[i]->GetIndex();
-            //nombres y posicion de los objetos 
             switch (pos_vector) 
             {
             case 0:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Horizontal con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Horizontal - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 1:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Vertical con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: " ) + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Vertical - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 2:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 3:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L invertida - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 4:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cuadrado con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cuadrado - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 5:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 6:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza T invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza T - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 7:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z - Numero de pieza: ") + FString::FromInt(i+1));
                 break;
             case 8:
-                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cruz con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cruz - Numero de pieza: ") + FString::FromInt(i+1));
             }
         }
     }
