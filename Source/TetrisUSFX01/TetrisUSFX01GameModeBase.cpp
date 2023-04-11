eb25e30ed53bc5aea58c69c9fd6d059274006e16
Samuel Albornoz <116126100+Salexo-28@users.noreply.github.com>
2023-04-10 22:42:22 -0400

    Update TetrisUSFX01GameModeBase.cpp

--- Source/TetrisUSFX01/TetrisUSFX01GameModeBase.cpp
+++ Source/TetrisUSFX01/TetrisUSFX01GameModeBase.cpp
@@ -2,32 +2,85 @@
 
 
 #include "TetrisUSFX01GameModeBase.h"
-
+//
 void ATetrisUSFX01GameModeBase::BeginPlay()
 {
-    // Call the parent class version of this function
+    //ciclo que genera bloques
+    GetWorldTimerManager().SetTimer(Time_Controller, this, &ATetrisUSFX01GameModeBase::Spawn_Bloque, TIntervalo, true);
+
     Super::BeginPlay();
-    // Displays a red message on the screen for 10 seconds
-    GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, TEXT("Spawning un Block"));
-    // Spawn an instance of the AMyFirstActor class at the
-    //default location.
-    FTransform SpawnLocation;
-    SpawnedActor = GetWorld()->SpawnActor<ABlock>(ABlock::StaticClass(), SpawnLocation);
-    SpawnedActor->SetActorRelativeLocation(FVector(0.0, 0.0, 0.0));
 
-    GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, TEXT("Spawning un Piece"));
-    SpawnedPiece = GetWorld()->SpawnActor<APiece>(APiece::StaticClass(), SpawnLocation);
-    SpawnedPiece->SetActorRelativeLocation(FVector(0.0, 100.0, 0.0));
-    SpawnedPiece->SpawnBlocks();
 
+}
+//funcion que genera los bloques
+void ATetrisUSFX01GameModeBase::Spawn_Bloque()
+{
+
+    TInicial = TInicial + TIntervalo;
+    if (TInicial < TMaximo)
+    {
+        positionZ = positionZ + 120;
+        GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Orange , TEXT("Bloque generado"));
+        FTransform SpawnLocation;
+
+        SpawnedPiece = GetWorld()->SpawnActor<APiece>(APiece::StaticClass(), SpawnLocation);
+        SpawnedPiece->SetActorRelativeLocation(FVector(positionX, positionY, positionZ));
+        SpawnedPiece->SpawnBlocks();
+        SpawnedPiece->setPositionX(positionX);
+        SpawnedPiece->setPositionY(positionY);
+        SpawnedPiece->setPositionZ(positionZ);
+            //guarda el nombre y posicion de la pieza
+        mpcontenedor.Add(cont_map, SpawnedPiece);
+        cont_map++;
+    }
+    else
+    {
+        GetWorldTimerManager().ClearTimer(Time_Controller);//cancela el temporizador
+        GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Orange, TEXT("Tiempo Cumplido"));
+        //muestra el Tmap
+        for (int i = 0; i < cont_map; i++)
+        {
+            pos_vector = mpcontenedor[i]->GetIndex();
+            //nombres y posicion de los objetos 
+            switch (pos_vector) 
+            {
+            case 0:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Horizontal con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 1:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza linea Vertical con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: " ) + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 2:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 3:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza L invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 4:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cuadrado con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 5:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 6:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza T invertida con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 7:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Z con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+                break;
+            case 8:
+                GEngine->AddOnScreenDebugMessage(-1, 25, FColor::Yellow, TEXT("Pieza Cruz con posicion en x: ") + FString::FromInt(mpcontenedor[i]->getPositionX()) + TEXT(", en y: ") + FString::FromInt(mpcontenedor[i]->getPositionY()) + TEXT(", en z: ") + FString::FromInt(mpcontenedor[i]->getPositionZ()));
+            }
+        }
+    }
 }
 
 void ATetrisUSFX01GameModeBase::DestroyActorFunction()
 {
-	if (SpawnedActor != nullptr)
-   {
-      // Displays a red message on the screen for 10 seconds
-      GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, TEXT("Actor Block destruido"));
-      SpawnedActor->Destroy();
-   }
+    if (SpawnedActor != nullptr)
+    {
+        // Displays a red message on the screen for 10 seconds
+        GEngine->AddOnScreenDebugMessage(-1, 10, FColor::Red, TEXT("Actor Block destruido"));
+        SpawnedActor->Destroy();
+    }
 }
